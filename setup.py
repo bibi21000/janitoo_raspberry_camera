a2c2dead776789e2f7f33a18eb28d967fed4152c
bibi21000 <bibi21000@gmail.com>
2016-01-26 19:58:24 +0100

    Split camera

--- setup.py
+++ setup.py
@@ -58,12 +58,12 @@ data_files_config(data_files, 'docs','src/docs/','*')
 #It will be used to collect entries without installing the package
 janitoo_entry_points = {
     "janitoo.threads": [
-        "picamera = janitoo_raspberry.thread_camera:make_thread",
+        "rpicamera = janitoo_raspberry.thread_camera:make_thread",
     ],
     "janitoo.components": [
-        "picamera.photo = janitoo_raspberry.camera:make_photo",
-        "picamera.video = janitoo_raspberry.camera:make_video",
-        "picamera.stream = janitoo_raspberry.camera:make_stream",
+        "rpicamera.photo = janitoo_raspberry.camera:make_photo",
+        "rpicamera.video = janitoo_raspberry.camera:make_video",
+        "rpicamera.stream = janitoo_raspberry.camera:make_stream",
     ],
 }
 
